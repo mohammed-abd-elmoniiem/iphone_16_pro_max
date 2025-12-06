9ab3b319464f263f1aa39f721aeaf51460e57baf
mohamed abd elmoniem <mohammed.abd.elmoniiem@gmail.com>
2025-12-06 14:48:08 +0200

    initialization

---
+++ src/main.js
@@ -0,0 +1,202 @@
+import './style.css'
+
+import  * as THREE from 'three'
+import { OrbitControls, FlyControls, EffectComposer, RenderPass, GTAOPass, GlitchPass } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+import * as CANNON from 'cannon-es'
+
+import GUI from 'lil-gui';
+import gsap from 'gsap';
+// import { button } from 'motion/react-client';
+
+
+// gui ++++++++++++++++++++++++++++++++++++++++++
+
+const gui = new GUI()
+
+
+
+
+
+
+
+const canvasElement = document.getElementById('canvas');
+
+const canvasSize ={
+    width:function(){return canvasElement.getBoundingClientRect().width},
+    height:function(){return canvasElement.getBoundingClientRect().height},
+    aspect:function(){return this.width()/this.height()},
+
+
+}
+
+
+
+// intialize scene ,camera , renderer++++++++++++++++++++++++++++++++++++++++++
+
+const scene = new THREE.Scene()
+scene.fog = new THREE.Fog(0x888888,1,30)
+// scene.overrideMaterial = new THREE.MeshDepthMaterial()
+
+ const frutm = 20
+const camera = new  THREE.PerspectiveCamera(90,canvasSize.aspect(),0.1,2000)
+// const camera = new  THREE.OrthographicCamera(-0.5*frutm , 0.5*frutm ,-0.5*frutm ,0.5*frutm,0.1,2000)
+
+camera.position.set(0,0,6)
+camera.lookAt(new THREE.Vector3(0,0,0))
+scene.add(camera)
+
+
+const renderer = new THREE.WebGLRenderer({antialias:true , canvas:canvasElement});
+renderer.setSize(canvasSize.width(),canvasSize.height());
+renderer.setPixelRatio ( window.devicePixelRatio);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace
+renderer.render(scene,camera)
+
+const renderTarget = new THREE.WebGLRenderTarget(800,600,{
+    samples:30
+})
+
+// renderer.shadowMap.enabled = true
+
+const effectComposer = new EffectComposer(renderer,renderTarget);
+
+const renderPass = new RenderPass(scene,camera);
+effectComposer.addPass(renderPass);
+
+const gtaPass = new GTAOPass(scene,camera);
+// effectComposer.addPass(gtaPass)
+
+// const glitch = new GlitchPass();
+
+// effectComposer.addPass(glitch)
+
+
+// --------------------------------------------------------------------------
+
+
+// loader ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const glbLoader = new GLTFLoader()
+const TXLoader = new THREE.TextureLoader()
+// --------------------------------------------------------------------
+
+
+
+// objects+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+glbLoader.load('/models/iphone_16_pro_max.glb',
+  model=>{
+  console.log(model)
+  scene.add(model.scene);
+
+  const tl = gsap.timeline();
+gsap.to(model.scene.position,{
+  duration:2,
+  z:5
+})
+
+gsap.to(model.scene.rotation,{
+  duration:2,
+  x:-0.5,
+  y:5,
+  // z:5
+})
+
+}
+,process=>{
+  console.log('progress=>'+ String(Math.round((process.loaded/process.total)*100)).padStart(3,' ') +'%')
+},
+error=>{
+  console.log(error)
+})
+
+
+// --------------------------------------------------------------------objects
+
+// controls +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const control = new OrbitControls(camera,canvasElement);
+
+control.update()
+
+
+// -----------------------------------------------------------------controls
+
+
+// gsap animation 
+
+
+
+
+//  animation +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const clock = new THREE.Clock()
+let eTime = 0
+function animate(){
+    // world.step(1/60,clock.getDelta(),3)
+//  console.log(sphereBody.position)
+
+    eTime= clock.getElapsedTime()
+
+    
+    // console.log(eTime , clock.getDelta());
+control.update(clock.getDelta())
+
+
+    // camera.lookAt(new THREE.Vector3(0,0,0))
+
+    // renderer.render(scene,camera);
+    effectComposer.render(scene,camera)
+    requestAnimationFrame(animate)
+}
+animate()
+
+// --------------------------------------------------------------------animation
+
+
+// lights +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const dirLight = new THREE.DirectionalLight(0xffffff,2)
+dirLight.position.set(5,10,5);
+dirLight.castShadow =true
+
+const dirLight2 = new THREE.DirectionalLight(0xffffff,1.5)
+dirLight2.position.set(-5,3,-5);
+dirLight2.castShadow =true
+
+
+const amLight = new THREE.AmbientLight(0xffffff,3)
+
+const pointLight = new THREE.PointLight(0xffad14,3,6);
+pointLight.castShadow=true;
+
+const hemiLight = new THREE.HemisphereLight(0xffeeff,0x001111,2);
+
+
+scene.add(dirLight ,dirLight2,hemiLight)
+
+
+// --------------------------------------------------------------------------lights
+
+
+
+
+
+// resize ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+window.addEventListener('resize',(eve)=>{
+    canvasElement.style.cssText = 'width:100vw ;height:100vh';
+      console.log(canvasSize.aspect())
+    camera.aspect = canvasSize.aspect()
+    renderer.setSize(canvasSize.width(),canvasSize.height())
+    renderer.setPixelRatio = Math.min(window.devicePixelRatio , 2);
+    camera.updateProjectionMatrix()
+    renderer.render(scene,camera)
+    
+})
+
+
+
+// --------------------------------------------------------------------------------
---
+++ src/mainGSAP.js
@@ -0,0 +1,74 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './style.css'
+import gsap from 'gsap'
+import { SplitText } from 'gsap/all'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const titleSplit = SplitText.create('#title',{type:'chars'
+})
+
+console.log(titleSplit)
+gsap.fromTo(titleSplit.chars,{
+    y:100,
+    opacity:1,
+
+},{
+    // y:'random(-100,100)',
+    // x:'random(-10,10)',
+    // scale:'random(0.2,3)',
+    repeat:5,
+   
+    opacity:1,
+    rotateZ:360,
+    yoyo:true,
+    duration:2,
+    stagger:0.1,
+    delay:1,
+    color:'#ff55cc',
+    ease:'bounce',
+    onComplete:()=>{
+        titleSplit.revert()
+    }
+})
+
+
+const tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:'#box',
+        // pin:true,
+        start:'bottom 50%',
+        end:'+=500',
+        scrub:true ,
+        snap:{
+        snapTo:'labels',
+        
+    },
+    ease:'elastic'
+   
+    }
+   
+
+   
+
+})
+
+tl.addLabel('start').to('#box',{scale:1.2,rotation:360,duration:1,translateY:200,ease:'bounce.out'})
+.addLabel('color').to('#box',{
+    backgroundColor:'#ff00ff',
+    x:200,
+    borderRadius:50,
+    duration:1
+}).to('#box',{
+    scale:2,
+    ease:'bounce.inOut',
+    duration:1
+})
+.to('#box',{
+    x:0,
+    y:0,
+    scale:0
+})
